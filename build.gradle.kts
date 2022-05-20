83c5fa405a15b34752f184d268144b276b0af68e
ogane <>
2022-05-21 00:10:53 +0900

    workflowファイルを編集した🛠

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ compose.desktop {
     application {
         mainClass = "MainKt"
         nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            targetFormats(TargetFormat.Dmg, TargetFormat.Exe, TargetFormat.Deb, TargetFormat.Rpm)
             packageName = "AIT-busInfo"
             packageVersion = "1.0.0"
         }
